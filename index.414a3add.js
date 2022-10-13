6dda57230803a8e8f7e4b02a219f83bddde3d720
Titotrapatonni <109283021+Titotrapatonni@users.noreply.github.com>
2022-10-13 13:55:02 +0000

    Deploying to gh-pages from @ Titotrapatonni/ice-cream-code-ninjas@7d98c60ffd36ed953f9555b4300df53bd5f9c17c 🚀

---
+++ index.414a3add.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-product3-open]"),closeModalBtn:document.querySelector("[data-modal-product-close]"),modal:document.querySelector("[data-modal-product]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.414a3add.js.map
---
+++ index.8c15659e.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-product2-open]"),closeModalBtn:document.querySelector("[data-modal-product-close]"),modal:document.querySelector("[data-modal-product]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.8c15659e.js.map
