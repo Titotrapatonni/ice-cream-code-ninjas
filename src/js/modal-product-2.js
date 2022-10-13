7d98c60ffd36ed953f9555b4300df53bd5f9c17c
Titotrapatonni <titovtelexfree@gmail.com>
2022-10-13 16:54:04 +0300

    connect modal product

---
+++ src/js/modal-product-2.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-product2-open]'),
+    closeModalBtn: document.querySelector('[data-modal-product-close]'),
+    modal: document.querySelector('[data-modal-product]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
