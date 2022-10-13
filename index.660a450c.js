603039e462a1da27b7debc5c1235afbbd6938ce4
Titotrapatonni <109283021+Titotrapatonni@users.noreply.github.com>
2022-10-13 22:48:21 +0000

    Deploying to gh-pages from @ Titotrapatonni/ice-cream-code-ninjas@a481626517e595c06e62da7c923310478b542a22 🚀

---
+++ index.660a450c.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-buy-mob-menu-open]"),closeModalBtn:document.querySelector("[data-modal-buy-close-mob-menu]"),modal:document.querySelector("[data-modal-buy]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.660a450c.js.map
